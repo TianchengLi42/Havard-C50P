fdb4faf18cee805275471e30c87fcc6d6de03cc9
Tiancheng Li <t366li@uwaterloo.ca>
2025-08-05 15:55:15 -0400

    Delete Deep Thought.py

--- Deep Thought.py
+++
@@ -1,9 +0,0 @@
-def check(insert):
-    if insert == "42":
-        return True
-    elif insert.casefold() == "forty two":
-        return True
-    else:
-        return False
-
-print(check(input("What is the answer to the great question of life, the universe, and everything ")))
